50f4ed9899cf1df989afcfdd2dab40722f5844c3
Jeany de Vries <jeanydevries@hotmail.com>
2022-03-21 14:13:03 +0100

    load app and css

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const path = require('path');
+const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/public/views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) =>{
+  res.render("index");
+})
+
+app.listen(port);
+
+/*
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.writeHead(200, {'Content-Type' : 'text/html'})
+  fs.readFile('index.html', function(error, data){
+    if(error){
+      res.writeHead(404);
+      res.write('Error: File not found')
+    }
+    else{
+      res.write(data);
+    }
+    res.end();
+  })
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
