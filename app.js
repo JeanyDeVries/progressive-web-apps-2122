29f8b3ab7875e4c2e74d145720eca7bd4ac6bd48
Jeany <jeanydevries@hotmail.com>
2022-03-25 16:03:42 +0100

    added a detail page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const api_key = "ixmhN4my&"
 
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
@@ -11,19 +12,33 @@ app.set('views', path.join(__dirname, '/public/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  fetch('https://www.rijksmuseum.nl/api/nl/collection?key=ixmhN4my&ps=20&imgonly=true')
+  fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=${api_key}&ps=20&imgonly=true`)
     .then(async response => {
       const artWorks = await response.json()
       res.render('index', {
-          pageTitle: 'ArtAtHomev2', 
+          pageTitle: 'Home page', 
           data: artWorks.artObjects
       })
     })
     .catch(err => res.send(err))
 })
 
+app.get('/painting/:id', (req, res) => {
+  fetch(`https://www.rijksmuseum.nl/api/nl/collection/${req.params.id}?key=${api_key}&imgonly=true`)
+      .then(async response => {
+         const detail = await response.json()
+         console.log(detail);
+          res.render('detail', {
+              pageTitle: `Kunstwerk: ${req.params.id}`,
+              data: detail.artObject
+          })
+      })
+      .catch(err => res.send(err))
+})
+
 app.listen(port);
 
+//Use later when everything works
 async function fetchJson(url) {
   return await fetch(url)
     .then((response) => response.json())
