262b3e1d2638f72a2aaa7dfe31a26880a764e5cd
Jeany <jeanydevries@hotmail.com>
2022-03-25 13:38:54 +0100

    images get loaded on index page

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-import { renderHTML } from './public/modules/renderHTML.js';
-
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
 app.set('view engine', 'ejs');
@@ -12,14 +10,23 @@ app.set('views', path.join(__dirname, '/public/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) =>{
-  res.render("index");
-})
-
-app.get('/paintings', function (req, res) {
-  fetchJson('https://www.rijksmuseum.nl/api/nl/collection?key=ixmhN4my&ps=20&imgonly=true').then(function (jsonData) {
-    renderHTML(jsonData);
-  })
+app.get('/', function (req, res) {
+  fetch('https://www.rijksmuseum.nl/api/nl/collection?key=ixmhN4my&ps=20&imgonly=true')
+    .then(async response => {
+      const artWorks = await response.json()
+      res.render('index', {
+          pageTitle: 'ArtAtHomev2', 
+          data: artWorks.artObjects
+      })
+    })
+    .catch(err => res.send(err))
 })
 
 app.listen(port);
+
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .then((body) => body.data)
+    .catch((error) => error)
+}
