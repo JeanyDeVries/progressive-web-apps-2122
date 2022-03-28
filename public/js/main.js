493b3a0ce6923a520ae571a48a70f341ed997401
Jeany de Vries <jeanydevries@hotmail.com>
2022-03-28 14:41:21 +0200

    manifest works

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('public/js/service-worker.js')
       .then(function(registration) {
         return registration.update();
         })
